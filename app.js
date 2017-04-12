77757d6475acab54c397fd4e1dba50d2ca5b8ad9
seulkeepark <qud9787@naver.com>
2017-04-12 21:52:55 +0900

    facebook google naver ouath api

--- app.js
+++ app.js
@@ -2,19 +2,16 @@ var express = require('express');
 var session = require('express-session');
 var MySQLStore = require('express-mysql-session')(session);
 var bodyParser = require('body-parser');
-var bkfd2Password = require("pbkdf2-password");
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
 var FacebookStrategy = require('passport-facebook').Strategy;
 var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
 var NaverStrategy = require('passport-naver').Strategy;
-var hasher = bkfd2Password();
 var mysql = require('mysql');
 var conn = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : 'qlrqodtmfrl2',
-  database : 'o2'
+  database : 'team25'
 });
 conn.connect();
 var app = express();
@@ -29,34 +26,12 @@ app.use(session({
     port:3306,
     user:'root',
     password:'qlrqodtmfrl2',
-    database:'o2'
+    database:'team25'
   })
 }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.get('/auth/logout', function(req, res){
-  req.logout();
-  req.session.save(function(){
-    res.redirect('/welcome');
-  });
-});
-app.get('/welcome', function(req, res){
-  if(req.user && req.user.displayName) {
-    res.send(`
-      <h1>Hello, ${req.user.displayName}</h1>
-      <a href="/auth/logout">logout</a>
-    `);
-  } else {
-    res.send(`
-      <h1>Welcome</h1>
-      <ul>
-        <li><a href="/auth/login">Login</a></li>
-        <li><a href="/auth/register">Register</a></li>
-      </ul>
-    `);
-  }
-});
 passport.serializeUser(function(user, done) {
   console.log('serializeUser : ', user);
   done(null, user.authId);
@@ -73,27 +48,8 @@ passport.deserializeUser(function(id, done) {
     }
   });
 });
-passport.use(new LocalStrategy(
-  function(username, password, done){
-    var uname = username;
-    var pwd = password;
-    var sql = 'SELECT * FROM users WHERE authId=?';
-    conn.query(sql, ['local:'+uname], function(err, results){
-      if(err){
-        return done('There is no user.');
-      }
-      var user = results[0];
-      return hasher({password:pwd, salt:user.salt}, function(err, pass, salt, hash){
-        if(hash === user.password){
-          console.log('LocalStrategy', user);
-          done(null, user);
-        } else {
-          done(null, false);
-        }
-      });
-    });
-  }
-));
+
+
 passport.use(new FacebookStrategy({
     clientID: '109125169639363',
     clientSecret: 'e7b147e18eaaa000c90f9c5580889a1f',
@@ -102,7 +58,6 @@ passport.use(new FacebookStrategy({
   function(accessToken, refreshToken, profile, done) {
     console.log(profile);
     var authId = 'facebook:'+profile.id;
-    var displayName = profile.displayName;
     var sql = 'SELECT * FROM users WHERE authId=?';
     conn.query(sql, [authId], function(err, results){
       if(results.length>0){
@@ -110,7 +65,7 @@ passport.use(new FacebookStrategy({
       } else {
         var newuser = {
           'authId':authId,
-          'displayName':displayName
+          'displayName':profile.displayName
         };
         var sql = 'INSERT INTO users SET ?';
         conn.query(sql, newuser, function(err, results){
@@ -133,7 +88,6 @@ passport.use(new GoogleStrategy({
   function(accessToken, refreshToken, profile, done) {
     console.log(profile);
     var authId = 'google:'+profile.id;
-    var displayName = profile.displayName;
     var sql = 'SELECT * FROM users WHERE authId=?';
     conn.query(sql, [authId], function(err, results){
       if(results.length>0){
@@ -141,7 +95,7 @@ passport.use(new GoogleStrategy({
       } else {
         var newuser = {
           'authId':authId,
-          'displayName':displayName
+          'displayName':profile.displayName
         };
         var sql = 'INSERT INTO users SET ?';
         conn.query(sql, newuser, function(err, results){
@@ -164,8 +118,6 @@ passport.use(new NaverStrategy({
     function(accessToken, refreshToken, profile, done) {
       console.log(profile);
       var authId = 'naver:'+profile.id;
-      var displayName = profile.emails[0].value;
-      console.log(displayName);
       var sql = 'SELECT * FROM users WHERE authId=?';
       conn.query(sql, [authId], function(err, results){
         if(results.length>0){
@@ -173,7 +125,7 @@ passport.use(new NaverStrategy({
         } else {
           var newuser = {
             'authId':authId,
-            'displayName':displayName
+            'displayName':profile.emails[0].value
           };
           var sql = 'INSERT INTO users SET ?';
           conn.query(sql, newuser, function(err, results){
@@ -188,17 +140,6 @@ passport.use(new NaverStrategy({
       });
     }
 ));
-app.post(
-  '/auth/login',
-  passport.authenticate(
-    'local',
-    {
-      successRedirect: '/welcome',
-      failureRedirect: '/auth/login',
-      failureFlash: false
-    }
-  )
-);
 app.get(
   '/auth/facebook',
   passport.authenticate(
@@ -252,74 +193,33 @@ app.get(
   	res.redirect('/welcome');
   }
 );
-app.post('/auth/register', function(req, res){
-  hasher({password:req.body.password}, function(err, pass, salt, hash){
-    var user = {
-      authId:'local:'+req.body.username,
-      username:req.body.username,
-      password:hash,
-      salt:salt,
-      displayName:req.body.displayName
-    };
-    var sql = 'INSERT INTO users SET ?';
-    conn.query(sql, user, function(err, results){
-      if(err){
-        console.log(err);
-        res.status(500);
-      } else {
-        req.login(user, function(err){
-          req.session.save(function(){
-            res.redirect('/welcome');
-          });
-        });
-      }
-    });
+app.get('/auth/logout', function(req, res){
+  req.logout();
+  req.session.save(function(){
+    res.redirect('/welcome');
   });
 });
-app.get('/auth/register', function(req, res){
-  var output = `
-  <h1>Register</h1>
-  <form action="/auth/register" method="post">
-    <p>
-      <input type="text" name="username" placeholder="username">
-    </p>
-    <p>
-      <input type="password" name="password" placeholder="password">
-    </p>
-    <p>
-      <input type="text" name="displayName" placeholder="displayName">
-    </p>
-    <p>
-      <input type="submit">
-    </p>
-  </form>
-  `;
-  res.send(output);
+app.get('/welcome', function(req, res){
+  if(req.user && req.user.displayName) {
+    res.send(`
+      <h1>Hello, ${req.user.displayName}</h1>
+      <a href="/auth/logout">logout</a>
+    `);
+  } else {
+    res.send(`
+      <h1>Welcome</h1>
+      <ul>
+        <li><a href="/auth/login">Login</a></li>
+      </ul>
+    `);
+  }
 });
 app.get('/auth/login', function(req, res){
   var output = `
   <h1>Login</h1>
-  <form action="/auth/login" method="post">
-    <p>
-      <input type="text" name="username" placeholder="username">
-    </p>
-    <p>
-      <input type="password" name="password" placeholder="password">
-    </p>
-    <p>
-      <input type="submit">
-    </p>
-  </form>
-  <p>
-  <a href="/auth/facebook">Sign In with facebook</a>
-  </p>
-  <p>
-  <a href="/auth/google">Sign In with Google</a>
-  </p>
-  <p>
-  <a href="/auth/naver">Sign In with Naver</a>
-  </p>
-  `;
+  <p><a href="/auth/facebook">Sign In with facebook</a></p>
+  <p><a href="/auth/google">Sign In with Google</a></p>
+  <p><a href="/auth/naver">Sign In with Naver</a></p>`;
   res.send(output);
 });
 app.listen(3003, function(){
